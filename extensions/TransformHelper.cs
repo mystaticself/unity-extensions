4ed5dbb40846dcbdf16c9c75cd7d55de652ab633
John Iacoviello <john@mystaticself.com>
2015-03-24 16:06:35 -0400

    helper additions

--- extensions/TransformHelper.cs
+++ extensions/TransformHelper.cs
@@ -23,6 +23,21 @@ public static class TransformHelper
 	{
 		transform.position = new Vector3(x, y, z);	
 	}
+	
+	public static void ScaleX(this Transform transform, float x)
+	{
+		transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
+	}
+	
+	public static void ScaleY(this Transform transform, float y)
+	{
+		transform.localScale = new Vector3(transform.localScale.x, y, transform.localScale.z);
+	}
+	
+	public static void ScaleZ(this Transform transform, float z)
+	{
+		transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, z);
+	}
 	#endregion
 	
 	#region Getters
--- extensions/Vector3Helper.cs
+++ extensions/Vector3Helper.cs
@@ -8,10 +8,73 @@ public static class Vector3Helper
 	{
 		return new Vector2(vector.x, vector.y);
 	}
-
+	
 	public static Vector2 YZ(this Vector3 vector)
 	{
 		return new Vector2(vector.y, vector.z);
+	}
+	
+	public static Vector3 PlusX(this Vector3 vector, float x)
+	{
+		Vector3 vec = new Vector3() + vector;
+		vec.x += x;
+		return vec;
+	}
+	
+	public static Vector3 PlusY(this Vector3 vector, float y)
+	{
+		Vector3 vec = new Vector3() + vector;
+		vec.y += y;
+		return vec;
+	}
+	
+	public static Vector3 PlusZ(this Vector3 vector, float z)
+	{
+		Vector3 vec = new Vector3() + vector;
+		vec.z += z;
+		return vec;
+	}
+	
+	public static Vector3 MinusX(this Vector3 vector, float x)
+	{
+		Vector3 vec = new Vector3() + vector;
+		vec.x -= x;
+		return vec;
+	}
+	
+	public static Vector3 MinusY(this Vector3 vector, float y)
+	{
+		Vector3 vec = new Vector3() + vector;
+		vec.y -= y;
+		return vec;
+	}
+	
+	public static Vector3 MinusZ(this Vector3 vector, float z)
+	{
+		Vector3 vec = new Vector3() + vector;
+		vec.z -= z;
+		return vec;
+	}
+	
+	public static Vector3 ScaleX(this Vector3 vector, float x)
+	{
+		Vector3 vec = new Vector3() + vector;
+		vec.x *= x;
+		return vec;
+	}
+	
+	public static Vector3 ScaleY(this Vector3 vector, float y)
+	{
+		Vector3 vec = new Vector3() + vector;
+		vec.y *= y;
+		return vec;
+	}
+	
+	public static Vector3 ScaleZ(this Vector3 vector, float z)
+	{
+		Vector3 vec = new Vector3() + vector;
+		vec.z *= z;
+		return vec;
 	}
 	#endregion
 }
